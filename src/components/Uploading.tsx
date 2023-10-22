c9c71046bfbe3069cc883defcd21fb5fc1a4aa3e
Christopher Tian <mechristophertian@gmail.com>
2023-10-22 10:50:38 -0300

    create uploading component

---
+++ src/components/Uploading.tsx
@@ -0,0 +1,9 @@
+export default function Uploading() {
+  return (
+    <div className="w-[400.36px] h-[143.57px] shadow-xl rounded-2xl bg-white">
+      <div className="m-6 flex flex-col gap-5">
+        <p>Uploading...</p>
+      </div>
+    </div>
+  );
+}

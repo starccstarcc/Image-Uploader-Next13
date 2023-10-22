56b3d5cd1d98fbfa7bfb5e73a8dcc932ef9b2032
Christopher Tian <mechristophertian@gmail.com>
2023-10-22 20:29:47 -0300

    use bar loader component

--- src/components/Uploading.tsx
+++ src/components/Uploading.tsx
@@ -1,8 +1,16 @@
+import { BarLoader } from "react-spinners";
+
 export default function Uploading() {
   return (
     <div className="w-[400.36px] h-[143.57px] shadow-xl rounded-2xl bg-white">
-      <div className="m-6 flex flex-col gap-5">
+      <div className="m-8 flex flex-col gap-7">
         <p>Uploading...</p>
+
+        <BarLoader
+          height={6}
+          width={330}
+          color="#2F80ED"
+        />
       </div>
     </div>
   );
